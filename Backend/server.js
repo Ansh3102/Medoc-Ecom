ce2f0d30932679aa89cbaf4cff623e5d1dd58c8a
Ansh3102 <anshulsharma2002.as@gmail.com>
2025-05-29 20:46:42 +0530

    Updated URL

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(cors({
 
   // server.js
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://medoc-ecom.vercel.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
